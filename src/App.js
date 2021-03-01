570bb76ca7c3ba051a3e8ad2bdd546fc5b553f65
siddharthgaglani <siddharthgaglani@gmail.com>
2021-03-02 00:20:54 +0530

    files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './App.css';
+import PlayerList from './Components/PlayerList';
+import SearchBar from './Components/SearchBar';
+import axios from 'axios';
+
+
+class App extends React.Component {
+
+  // onSearchSubmit(term){
+  //   console.log(term);
+  //   // axios.get(`https://api.npoint.io/d6bd0efc05639084eb17/`,{
+  //   //   params: {
+  //   //     query: term
+  //   //   }
+  //   // })
+  // }
+  
+  render() {
+    return (
+      <div className="App">
+        {/* <SearchBar onSubmit={this.onSearchSubmit} /> */}
+        <PlayerList />
+      </div>
+    );
+  }
 }
 
 export default App;
